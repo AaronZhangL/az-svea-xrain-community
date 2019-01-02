c1a52824657a76cbc8d523cbdff338ad64517dec
Aaron Zhang <aaronzhang@gmail.com>
2019-01-02 19:16:23 +0900

    Add selectbox item

--- portal/client/src/components/AddForm.js
+++ portal/client/src/components/AddForm.js
@@ -6,14 +6,16 @@ import {
   changeHltype
 } from '../actions'
 //xRain
-import Select from 'react-select';
+//import Select from 'react-select';
 
 const AddForm = ({ store }) => {
   const { name, age, hltype } = store.getState().form
+  //xRain Start
+
+  //xRain end
 
   const handleSubmit = e => {
     e.preventDefault()
-
     store.dispatch(requestData())
     axios.post('/api/characters', {
       name,
@@ -35,18 +37,18 @@ const AddForm = ({ store }) => {
     <div>
       <form onSubmit={e => handleSubmit(e)}>
         <label>
-        Human Language:
         <p>
-        <select name="hltype">
-        <option value="en-us" selected>English</option>
-        <option value="ja-jp">日本語</option>
-        <option value="zh-hans">简体中文</option>
-        <option value="zh-hant" disabled>繁體中文</option>
-        <option value="zh-cn" disabled>大陆简体</option>
-        <option value="zh-tw" disabled>台灣正體</option>
-        <option value="zh-hk" disabled>港澳繁體</option>
-        <option value="zh-sg" disabled>马新简体</option>
-        <option value="ko-kr" disabled>한국어</option>
+        Human Language:
+        <select value={hltype} onChange={e => store.dispatch(changeHltype(e.target.value))} >
+          <option value="en-us" defaultValue>English</option>
+          <option value="ja-jp">日本語</option>
+          <option value="zh-hans">简体中文</option>
+          <option value="zh-hant" disabled>繁體中文</option>
+          <option value="zh-cn" disabled>大陆简体</option>
+          <option value="zh-tw" disabled>台灣正體</option>
+          <option value="zh-hk" disabled>港澳繁體</option>
+          <option value="zh-sg" disabled>马新简体</option>
+          <option value="ko-kr" disabled>한국어</option>
         </select>
         </p>
         </label>
--- portal/client/src/components/App.js
+++ portal/client/src/components/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import AddForm from './AddForm'
 import CharacterList from './CharacterList'
+//xRain
+//import Select from 'react-select';
+
 
 class App extends Component {
+  //xRain
+  constructor(props) {
+    super(props);
+    this.state = {value: 'js-jp'};
+
+    //this.handleChange = this.handleChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   render() {
     return (
-      <div>
-        <AddForm store={this.props.store} />
-        <CharacterList store={this.props.store} />
-      </div>
+          <div>
+            <AddForm store={this.props.store} />
+            <CharacterList store={this.props.store} />
+          </div>
     );
   }
 }
