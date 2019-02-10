7a00d203d8755ab2e59a6c77abf9bc6ba7e9e17d
Aaron Zhang <aaronzhang@gmail.com>
2019-02-10 23:26:22 +0900

    Copy from saas project

---
+++ portal-trans/xrain-community/app/components/common/LoginButton.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import { makeQueryString } from '../../lib/api/makeQueryString';
+import env from '../../lib/env';
+
+import { styleLoginButton } from '../../lib/sharedStyles';
+
+// TS errors: https://github.com/mui-org/material-ui/issues/8198
+
+const dev = process.env.NODE_ENV !== 'production';
+const { PRODUCTION_URL_API } = env;
+const LOGIN_URL = dev ? 'http://localhost:8000' : PRODUCTION_URL_API;
+
+class LoginButton extends React.PureComponent<{ next?: string; invitationToken?: string }> {
+  public render() {
+    const { next, invitationToken } = this.props;
+
+    let url = `${LOGIN_URL}/auth/google`;
+    const qs = makeQueryString({ next, invitationToken });
+
+    if (qs) {
+      url += `?${qs}`;
+    }
+
+    return (
+      <Button variant="contained" style={styleLoginButton} href={url}>
+        <img src="https://storage.googleapis.com/async-await-all/G.svg" alt="Log in with Google" />
+        &nbsp;&nbsp;&nbsp; Log in with Google
+      </Button>
+    );
+  }
+}
+
+export default LoginButton;
