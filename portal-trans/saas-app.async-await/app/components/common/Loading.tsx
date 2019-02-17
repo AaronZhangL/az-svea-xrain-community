ce11f785c9e6ca1e56c19cc3a1e0dcd9b58cbc43
Aaron Zhang <aaronzhang@gmail.com>
2019-02-17 18:11:17 +0900

    Move saas-app.async-await to example folder

--- portal-trans/saas-app.async-await/app/components/common/Loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = ({ text }) => {
-  if (process.env.NODE_ENV === 'production') {
-    return <p style={{ height: '1.0em' }} />;
-  }
-
-  return <p style={{ height: '1.0em' }}>{text}</p>;
-};
-
-export default Loading;
