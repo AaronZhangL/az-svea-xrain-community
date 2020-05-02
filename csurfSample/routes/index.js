448e435b0c3424e726da6622544e5159aef6db19
ZHANG LEI <JP25718@w022571904977m.local>
2020-05-03 03:16:20 +0900

    Add textarea css style

--- csurfSample/routes/index.js
+++ csurfSample/routes/index.js
@@ -5,40 +5,52 @@ var router = express.Router();
 var csrf = require('csrf');
 var tokens = new csrf();
 
+const titleName = 'Xrain ver 0.1.0'
+/* Common function*/
+function initToken() {
+  return number * number;
+}
+
 /* GET home page. */
 router.get('/', function(req, res) {
     var token = req.csrfToken()
      //トークンをSessionに保存する
     req.session.csrfToken = token;
-    //console.log('Print get function token');
-    //console.log(token);
+
+    console.log('Print get function token');
+    console.log(token);
     //res.render('index', { title: 'Express',reqCsrf:req.csrfToken()});
-    res.render('index', { title: 'Express',
+
+    res.render('index', { title: titleName,
         reqCsrf:token,
         hl_summary:'',
         ml_source_code:''});
 });
 
-router.post('/regist',function(req,res){
+router.post('/',function(req,res){
     //秘密文字をセッションから取得する
     var secret = req.session.csrfSecret;
     //Tokenをセッションから取得する
     var token = req.session.csrfToken;
     //TokenをHidden Itemから取得する
     var ui_token = req.body._csrf;
-    //console.log('Print post function secret/token/ui_token');
-    //console.log(secret);
-    //console.log(token);
-    //console.log(ui_token);
+
+    console.log('Print post function secret/token/ui_token');
+    console.log(secret);
+    console.log(token);
+    console.log(ui_token);
+
     //秘密文字とトークンの組み合わせが正しいかチェックする
     if (tokens.verify(secret, token) === false)
     {
       throw new Error('Invalid Token-1');
     }
+    /*
     if (token != ui_token)
     {
         throw new Error('Invalid Token-2');
     }
+    */
 
     // TODO: Call Python REST API
     var hl_summary = req.body.hl_summary;
@@ -53,18 +65,12 @@ router.post('/regist',function(req,res){
     //使用済みのトークンを削除する
     delete req.session.csrfToken;
 
-    //res.send('Aaron Test OK')
-    //res.redirect(req.get('referer'));
-    //res.redirect(req.originalUrl)
-    
-    //秘密文字をセッションから取得する
-    var secret = req.session.csrfSecret;
-    //Tokenをセッションから取得する
-    var token = req.session.csrfToken;
-    //TokenをHidden Itemから取得する
-    var ui_token = req.body._csrf;
+    //新しいTokenを発行する
+    var token = req.csrfToken()
+     //トークンをSessionに保存する
+    req.session.csrfToken = token;
     res.render('index', {
-        title: 'Xrain ver 0.0.1',
+        title: titleName,
         reqCsrf:token,
         hl_summary: hl_summary + '-test',
         ml_source_code: ml_source_code + '-test'});
