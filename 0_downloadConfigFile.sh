21414b447e06db9bd0d9a119ca049b29bf6d12c7
ZHANG LEI <JP25718@w022571904977m.local>
2021-05-27 17:31:23 +0900

    Update download config file script

--- 0_downloadConfigFile.sh
+++ 0_downloadConfigFile.sh
@@ -1,5 +1,5 @@
 source .az-svea-xrain-config.token.source
-wget https://raw.githubusercontent.com/AaronZhangL/az-svea-xrain-config/master/az-svea-xrain-community/config/config.json\?token\=AAZ46IU5HODCCP35LAFDEPDAXBI3I -O ./config/config.json
-wget https://raw.githubusercontent.com/AaronZhangL/az-svea-xrain-config/master/az-svea-xrain-community/config/log4js.json\?token\=AAZ46IRBYZYMMJJDYDYQX3TAXBI4M -O ./config/log4js.json
+wget $CONFIG_CONFIG_JSON -O ./config/config.json
+wget $CONFIG_LOG4JS_JSON -O ./config/log4js.json
 echo "====== Config file list ========"
 ls -al ./config/*.json
