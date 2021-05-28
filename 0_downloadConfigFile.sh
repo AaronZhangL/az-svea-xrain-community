7edb0816c75bab59b0f294a5308b9cba9ec40af0
ZHANG LEI <JP25718@w022571904977m.local>
2021-05-28 10:17:13 +0900

    Update download remote config file scripit

--- 0_downloadConfigFile.sh
+++
@@ -1,5 +0,0 @@
-source .az-svea-xrain-config.token.source
-wget $CONFIG_CONFIG_JSON -O ./config/config.json
-wget $CONFIG_LOG4JS_JSON -O ./config/log4js.json
-echo "====== Config file list ========"
-ls -al ./config/*.json
